5fcecc4173b157edf459d844e503864819c8f380
Matthew McGowan <mat.mcgowan+git@gmail.com>
2022-06-28 18:23:04 -0700

    adds CI build for the example.

---
+++ .github/actions/compile-examples/compile.sh
@@ -0,0 +1,18 @@
+set -e
+SKETCH="$1"
+BOARD="$2"
+LIBS="$3"
+
+cp -r . /home/blues/Arduino/libraries/Blues_Wireless_Notecard
+HOME=/home/blues
+IFS=';'
+read -ra libraries <<< "$LIBS"
+for lib in "${libraries[@]}"; do arduino-cli lib install $lib; done
+
+arduino-cli compile \
+      --build-property compiler.cpp.extra_flags='-Wno-unused-parameter -Werror' \
+      --fqbn "$BOARD" \
+      --log-level trace \
+      --verbose \
+      --warnings all \
+      "$SKETCH"
